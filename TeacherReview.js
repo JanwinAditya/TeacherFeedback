412705d85de79283c4fa93a37233380ea8d76c61
Aditya-Chaudary05 <140259231+JanwinAditya@users.noreply.github.com>
2024-12-19 11:45:09 +0530

    Add files via upload

---
+++ TeacherReview.js
@@ -0,0 +1,4 @@
+var myName=prompt("What is your name?");
+var review=prompt('Write your review about your DCCN teacher in 140 words');
+alert("You review is "+ review.length+" letters long")
+alert("Final review submitted is : "+ review.slice(0,140)); 
